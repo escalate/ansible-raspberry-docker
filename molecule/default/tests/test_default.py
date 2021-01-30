6ee0f7a19f3c6c2398d93bf29c04c7381d94985e
Felix Boerner <ich@felix-boerner.de>
2021-01-30 19:44:04 +0100

    feat: add molecule testing

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,23 @@
+"""Role testing files using testinfra"""
+
+
+def test_daemon_config(host):
+    """Check docker daemon config"""
+    f = host.file("/etc/docker/daemon.json")
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+
+def test_cron_job(host):
+    """Check cron job"""
+    cmd = "docker system prune --volumes --force"
+    f = host.file('/var/spool/cron/root/crontab').content_string
+    assert cmd in f
+
+
+def test_docker_service(host):
+    """Check docker service"""
+    s = host.service("docker")
+    assert s.is_running
+    assert s.is_enabled
